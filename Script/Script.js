bae392c40de87a6af75a728f4ecec04c6000f3bf
Elton <166235259+Iarlen12@users.noreply.github.com>
2025-05-02 16:16:47 -0300

    update Script/Script.js

--- Script/Script.js
+++ Script/Script.js
@@ -1,5 +1,12 @@
 function clickMenu(){
-  const menu = document.getElementByClass('menu')
+
+  const menu = document.getElementById('menu')
   
-  menu.style.display = 
+  if (menu.style.display === "block"){
+    menu.style.display = "none"
+  }
+  else{
+    menu.style.display = "block"
+  }
+  
 }
