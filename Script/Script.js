5c7097c3382d1c82f7873bdeb6e6d8a91308a3a3
Iarlen12 <iarlems36@gmail.com>
2025-05-17 11:21:49 -0300

    update

--- Script/Script.js
+++ Script/Script.js
@@ -1,21 +1,36 @@
+
+function carBar() {
+  const carBar = document.getElementById('carBar')
+  const close = document.getElementById('close')
+  const carrinho = document.getElementById('carrinho')
+  
+  carBar.classList.toggle('burger-ativo')
+  carrinho.classList.add('active')
+  
+  if (close) {
+    close.addEventListener('click', () =>{
+      carrinho.classList.remove('active')
+    })
+  }
+}
+document.getElementById('carBar').addEventListener('click', carBar)
+
+
 function clickMenu() {
   const menu = document.getElementById('menu');
-  const burger = document.getElementById('burger')
-    
-    
-    
-    burger.classList.toggle('burger-ativo')
-    
+  const burger = document.getElementById('burger');
+
+  burger.classList.toggle('burger-ativo');
+
   if (menu.style.display === 'block') {
     // Animação para fechar
-    
     menu.style.transition = 'opacity 0.3s ease, transform 0.3s ease';
     menu.style.opacity = '0';
     menu.style.transform = 'translateY(-20px)';
-    
+
     setTimeout(() => {
       menu.style.display = 'none';
-    }, 300); // Espera a animação terminar antes de esconder
+    }, 300);
   } else {
     // Mostra e anima para abrir
     menu.style.display = 'block';
